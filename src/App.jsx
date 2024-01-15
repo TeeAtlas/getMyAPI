ece098edee175589a1ebb4f33ff5008998300728
Tania Boterman <taniaboterman@gmail.com>
2024-01-15 14:12:03 +0100

    request to API now working

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,31 @@ import CountryInfo from './components/CountryInfo';
 import LocationMap from './components/LocationMap';
 
 function App() {
-  const [ip, setIp] = useState(null);
-  const [location, setLocation] = useState(null);
+  const [ip, setIp] = useState("");
+  const [location, setLocation] = useState({});
+  const apiKey = import.meta.env.VITE_API_KEY;
 
   useEffect(() => {
-    fetch('https://api.ipify.org?format=json')
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`)
     .then(response => response.json())
     .then(data => {
       setIp(data.ip);
-      console.log(data.ip);
-      navigator.geolocation.getCurrentPosition((position) => {
-        setLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        });
+      console.log(data);
+      // navigator.geolocation.getCurrentPosition((position) => {
+        // setLocation({
+        //     lat: position.coords.latitude,
+        //     lng: position.coords.longitude,
+        //   });
+        // });
       });
   }, []);
 
   return (
     <>
       <h1>MY IP</h1>
-      <p>Your IP is: none of your business </p>
-      <CountryInfo ip={ip}/>
-      <LocationMap location={location} />
+      <p>Your IP is: {ip} </p>
+      {/* <CountryInfo ip={ip}/>
+      <LocationMap location={location} /> */}
     </>
   );
 }
